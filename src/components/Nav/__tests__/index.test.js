99f849ef24cbfd2fd3dbe212e5c2bbcb02bb3563
smardone <shaun.mardones@gmail.com>
2020-10-14 21:27:00 -0400

    completed conditional rendering for contact form

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -9,6 +9,8 @@ const categories = [
   ]
   const mockCurrentCategory = jest.fn();
   const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -18,7 +20,10 @@ describe('Nav component', () => {
         render(<Nav 
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory}/>);
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+            />);
     });
 
     // snapshot test
@@ -26,7 +31,10 @@ describe('Nav component', () => {
         const {asFragment} = render(<Nav 
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory}/>);
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+            />);
 
         expect(asFragment()).toMatchSnapshot();
     })
@@ -37,7 +45,9 @@ describe('emoji is visible', () => {
         const { getByLabelText } = render(<Nav 
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory}/>);
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}/>);
 
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     })
@@ -48,7 +58,9 @@ describe('links are visible', () => {
       const { getByTestId } = render(<Nav 
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
-        currentCategory={mockCurrentCategory}/>);
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}/>);
       expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
       expect(getByTestId('about')).toHaveTextContent('About me');
     });
