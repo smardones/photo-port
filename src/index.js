d6552e63aa81573e4f37f071bfad15907090a54a
Shaun Mardones <65517328+smardones@users.noreply.github.com>
2020-10-17 20:05:26 -0400

    Update and rename index.js to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './src/index.css';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
